828a5e852c2c771849b9a6278e0cba3b12ae8b3d
lencx <cxin1314@gmail.com>
2021-03-13 13:56:39 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+struct Person;
+struct Name(String);
+struct GreetTimer(Timer);
+pub struct HelloPlugin;
+
+fn add_people(commands: &mut Commands) {
+    commands
+        .spawn((Person, Name("Elaina Proctor".to_string())))
+        .spawn((Person, Name("Renzo Hume".to_string())))
+        .spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(
+    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if the timer hasn't finished yet, we return
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
+
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        // the reason we call from_seconds with the true flag is to make the timer repeat itself
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
